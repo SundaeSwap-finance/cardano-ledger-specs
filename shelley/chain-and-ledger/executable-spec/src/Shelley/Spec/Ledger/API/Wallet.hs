672687213b731068da860c600c4b723d1d291c1f
Tim Sheard <tim.sheard@IOHK.io>
2021-07-07 10:06:21 -0400

    Refactored RewardSnapShot, and added Pulsing to SnapShots.
    Added pulsing on the tick rule, and calculation of how many slots per epoch.
    Changed the Eq class and Serialisation classes for SnapShots.
    They now complete the _pstakeMark when necessary.
    Fixed types for Tests, added arbitrary instances, and uses of SnapShots.
    Added ( HasField address (Core.TxOut era) (Addr (Crypto era)) ) to (WellFormed era)

--- shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/API/Wallet.hs
+++ shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/API/Wallet.hs
@@ -109,7 +109,7 @@ import Shelley.Spec.Ledger.UTxO (UTxO (..))
 -- This is not based on any snapshot, but uses the current ledger state.
 poolsByTotalStakeFraction ::
   forall era.
-  (UsesValue era, HasField "address" (Core.TxOut era) (Addr (Crypto era))) =>
+  UsesValue era =>
   Globals ->
   NewEpochState era ->
   PoolDistr (Crypto era)
@@ -144,8 +144,7 @@ getTotalStake globals ss =
 getNonMyopicMemberRewards ::
   ( UsesValue era,
     HasField "_a0" (Core.PParams era) NonNegativeInterval,
-    HasField "_nOpt" (Core.PParams era) Natural,
-    HasField "address" (Core.TxOut era) (Addr (Crypto era))
+    HasField "_nOpt" (Core.PParams era) Natural
   ) =>
   Globals ->
   NewEpochState era ->
@@ -218,9 +217,7 @@ getNonMyopicMemberRewards globals ss creds =
 -- do not want to use one of the regular snapshots, but rather the most recent
 -- ledger state.
 currentSnapshot ::
-  ( UsesValue era,
-    HasField "address" (Core.TxOut era) (Addr (Crypto era))
-  ) =>
+  UsesValue era =>
   NewEpochState era ->
   EB.SnapShot (Crypto era)
 currentSnapshot ss =
